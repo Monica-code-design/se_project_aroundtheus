a0edc28255b47f5df1797051d7412c90adabb213
Monica Gonzalez <monicagonzalez.dev@gmail.com>
2022-12-11 12:42:24 -0600

    Resolve code compilation errors

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,8 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-
 module.exports = {
-  devtool: "inline-source-map",
+  devtool: "source-map",
   entry: {
     main: "./src/pages/index.js",
   },
@@ -13,22 +12,27 @@ module.exports = {
     filename: "main.js",
     publicPath: "",
   },
-  target: ["web", "es5"],
-  stats: "errors-only",
+  //target: ["web", "es5"],
+  //stats: "errors-only",
   mode: "development",
   devServer: {
-    static: path.resolve(__dirname, "./dist"),
+    contentBase: path.resolve(__dirname, "./dist"),
     compress: true,
     port: 8080,
     open: true,
     liveReload: true,
-    hot: false,
+    //hot: false,
   },
   module: {
     rules: [
+      // this is an array of rules
+      // add an object containing rules for Babel to it
       {
+        // a regular expression that searches for all js files
         test: /\.js$/,
+        // all files must be processed by babel-loader
         loader: "babel-loader",
+        // exclude the node_modules folder, we don't need to process files in it
         exclude: "/node_modules/",
       },
       {
@@ -45,16 +49,26 @@ module.exports = {
         ],
       },
       {
-        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
         type: "asset/resource",
+        generator: {
+          filename: "images/[name].[hash][ext]",
+        },
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "fonts/[name].[hash][ext]",
+        },
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html"
+      template: "./src/index.html", // path to our index.html file
     }),
     new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin()
+    new MiniCssExtractPlugin(), // connect the plugin for merging CSS files
   ],
 };
