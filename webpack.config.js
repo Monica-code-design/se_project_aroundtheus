e08b2e3119fdd24943858d47947e3c067ad34481
Monica Gonzalez <monicagonzalez.dev@gmail.com>
2022-12-11 23:49:00 -0600

    Resolve code compilation errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,31 @@
-// webpack.config.js
-const path = require("path");
+const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  devtool: "source-map",
   entry: {
-    main: "./src/pages/index.js",
+    main: './src/page/index.js'
   },
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "main.js",
-    publicPath: "",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    publicPath: ''
   },
-  //target: ["web", "es5"],
-  //stats: "errors-only",
-  mode: "development",
+  devtool: 'inline-source-map',
+  mode: 'development',
   devServer: {
-    contentBase: path.resolve(__dirname, "./dist"),
+    static: path.resolve(__dirname, './dist'),
     compress: true,
     port: 8080,
     open: true,
-    //liveReload: true,
-    //hot: false,
   },
   module: {
     rules: [
-      // this is an array of rules
-      // add an object containing rules for Babel to it
       {
-        // a regular expression that searches for all js files
         test: /\.js$/,
-        // all files must be processed by babel-loader
         loader: "babel-loader",
-        // exclude the node_modules folder, we don't need to process files in it
-        exclude: "/node_modules/",
+        exclude: "/node_modules/"
       },
       {
         test: /\.css$/,
@@ -43,34 +33,22 @@ module.exports = {
           MiniCssExtractPlugin.loader,
           {
             loader: "css-loader",
-            options: {
-              importLoaders: 1,
-            },
+            options: { importLoaders: 1 }
           },
-          "postcss-loader",
-        ],
+          "postcss-loader"
+        ]
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/,
-        type: "asset/resource",
-        generator: {
-          filename: "images/[name].[hash][ext]",
-        },
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: "asset/resource"
       },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
-        generator: {
-          filename: "fonts/[name].[hash][ext]",
-        },
-      },
-    ],
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "/src/index.html", // path to our index.html file
+      template: "./src/index.html"
     }),
     new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin(), // connect the plugin for merging CSS files
-  ],
-};
+    new MiniCssExtractPlugin(),
+  ]
+}
